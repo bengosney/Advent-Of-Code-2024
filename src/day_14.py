5e6237e498186f1ca91d3326ba48f23d9e3e1310
Ben Gosney <bengosney@googlemail.com>
2024-12-14 10:03:39 +0000

    feat(day14): part 1

---
+++ src/day_14.py
@@ -0,0 +1,134 @@
+import re
+from collections import defaultdict
+from dataclasses import dataclass
+from functools import cached_property
+from math import prod
+
+from utils import no_input_skip, read_input
+
+
+@dataclass
+class Robot:
+    x: int
+    y: int
+    vx: int
+    vy: int
+
+    def move(self, extents: "Extent"):
+        self.x = (self.x + self.vx) % extents.x
+        self.y = (self.y + self.vy) % extents.y
+
+
+@dataclass(frozen=True)
+class Extent:
+    x: int
+    y: int
+
+    @cached_property
+    def lower_x(self) -> int:
+        return self.x // 2
+
+    @cached_property
+    def upper_x(self) -> int:
+        return self.lower_x + 1
+
+    @cached_property
+    def lower_y(self) -> int:
+        return self.y // 2
+
+    @cached_property
+    def upper_y(self) -> int:
+        return self.lower_y + 1
+
+    def quadrant(self, robot: Robot) -> int:
+        if robot.x < self.lower_x and robot.y < self.lower_y:
+            return 1
+        elif robot.x > self.lower_x and robot.y < self.lower_y:
+            return 2
+        elif robot.x < self.lower_x and robot.y > self.lower_y:
+            return 3
+        elif robot.x > self.lower_x and robot.y > self.lower_y:
+            return 4
+        else:
+            return 0
+
+
+def parse_input(puzzle: str) -> list[Robot]:
+    regex = re.compile(r"(-?\d+),(-?\d+)")
+
+    robots = []
+    for line in puzzle.splitlines():
+        parts = regex.findall(line)
+        px, py = int(parts[0][0]), int(parts[0][1])
+        vx, vy = (int(parts[1][0]), int(parts[1][1]))
+        robots.append(Robot(px, py, vx, vy))
+
+    return robots
+
+
+def part_1(puzzle: str, extents: Extent = Extent(101, 103)) -> int:
+    robots = parse_input(puzzle)
+
+    for _ in range(100):
+        for robot in robots:
+            robot.move(extents)
+
+    quadrants = defaultdict(int)
+    for robot in robots:
+        quadrants[extents.quadrant(robot)] += 1
+    del quadrants[0]
+
+    return prod(quadrants.values())
+
+
+def part_2(puzzle: str, extents: complex = complex(101, 103)) -> int:
+    pass
+
+
+# -- Tests
+
+
+def get_example_input() -> str:
+    return """p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3"""
+
+
+def test_part_1() -> None:
+    test_input = get_example_input()
+    assert part_1(test_input, Extent(11, 7)) == 12
+
+
+# def test_part_2() -> None:
+#     test_input = get_example_input()
+#     assert part_2(test_input) is not None
+
+
+@no_input_skip
+def test_part_1_real() -> None:
+    real_input = read_input(__file__)
+    assert part_1(real_input) == 208437768
+
+
+# @no_input_skip
+# def test_part_2_real() -> None:
+#     real_input = read_input(__file__)
+#     assert part_2(real_input) is not None
+
+
+# -- Main
+
+if __name__ == "__main__":
+    real_input = read_input(__file__)
+
+    print(f"Part1: {part_1(real_input)}")
+    print(f"Part2: {part_2(real_input)}")
