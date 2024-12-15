4307e22e72783a4c0a179270aa6b2169965b3962
Ben Gosney <bengosney@googlemail.com>
2024-12-15 20:36:55 +0000

    WIP(day15): part 2 passes example, fails the real input

--- src/day_15.py
+++ src/day_15.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
-from utils import no_input_skip, read_input  # noqa: F401
+from utils import no_input_skip, read_input
 
 Robot = complex
 Box = complex
@@ -20,6 +20,9 @@ class Warehouse:
     robot: Robot
     boxes: list[Box]
     walls: list[Wall]
+
+    double_wide: bool = False
+    box_links: dict[Box, Box] = field(default_factory=dict)
 
     def draw(self):
         min_x = min_y = 0
@@ -45,10 +48,11 @@ class Warehouse:
 
     def move(self, move: Move):
         new_robot = self.robot + move
+        move_box = self.move_box if not self.double_wide else self.move_box_double_wide
         if new_robot in self.walls:
             return False
         if new_robot in self.boxes:
-            if not self.move_box(move, new_robot):
+            if not move_box(move, new_robot):
                 return False
 
         self.robot = new_robot
@@ -68,30 +72,77 @@ class Warehouse:
 
         return True
 
+    def move_box_double_wide(self, move: Move, box: Box, checked: set[Box] = set()):
+        new_box = box + move
+
+        if box in checked:
+            return True
+
+        if new_box in self.walls:
+            return False
+
+        if new_box in self.boxes:
+            if not self.move_box_double_wide(move, new_box, checked | {box}):
+                return False
+
+        if move not in [move_map["<"], move_map[">"]]:
+            other_box = self.box_links[box]
+            if not self.move_box_double_wide(move, other_box, checked | {box}):
+                return False
+
+        self.boxes.remove(box)
+        self.boxes.append(new_box)
+
+        self.box_links[new_box] = self.box_links[box]
+        self.box_links[self.box_links[box]] = new_box
+        del self.box_links[box]
+
+        return True
+
     def score(self) -> int:
         score = 0
-        for box in self.boxes:
-            score += 100 * int(box.imag) + int(box.real)
+
+        def sort_complex(comp: complex):
+            return comp.real + (comp.imag * 1000)
+
+        scored = set()
+
+        for box in sorted(self.boxes, key=sort_complex):
+            if box not in scored:
+                scored.add(box)
+                try:
+                    scored.add(self.box_links[box])
+                except KeyError:
+                    pass
+                score += 100 * int(box.imag) + int(box.real)
         return score
 
 
-def parse_input(puzzle: str) -> tuple[Warehouse, list[Move]]:
+def parse_input(puzzle: str, double_wide: bool = False) -> tuple[Warehouse, list[Move]]:
     warehouse, moves = puzzle.split("\n\n")
     robot = complex(0, 0)
     boxes = []
     walls = []
+    box_links = {}
+    mul = 2 if double_wide else 1
     for y, line in enumerate(warehouse.splitlines()):
         for x, cell in enumerate(line):
-            pos = complex(x, y)
+            pos = complex(x * mul, y)
             if cell == "@":
                 robot = pos
             elif cell == "O":
                 boxes.append(pos)
+                if double_wide:
+                    boxes.append(pos + 1)
+                    box_links[pos + 1] = pos
+                    box_links[pos] = pos + 1
             elif cell == "#":
                 walls.append(pos)
+                if double_wide:
+                    walls.append(pos + 1)
 
     moves = [move_map[move] for move in moves.strip() if move in move_map]
-    return Warehouse(robot, boxes, walls), moves
+    return Warehouse(robot, boxes, walls, double_wide, box_links), moves
 
 
 def part_1(puzzle: str) -> int:
@@ -104,7 +155,14 @@ def part_1(puzzle: str) -> int:
 
 
 def part_2(puzzle: str) -> int:
-    pass
+    warehouse, moves = parse_input(puzzle, double_wide=True)
+
+    warehouse.draw()
+    for move in moves:
+        warehouse.move(move)
+    warehouse.draw()
+
+    return warehouse.score()
 
 
 # -- Tests
@@ -139,21 +197,21 @@ def test_part_1() -> None:
     assert part_1(test_input) is not None
 
 
-# def test_part_2() -> None:
-#     test_input = get_example_input()
-#     assert part_2(test_input) is not None
+def test_part_2() -> None:
+    test_input = get_example_input()
+    assert part_2(test_input) == 9021
 
 
-# @no_input_skip
-# def test_part_1_real() -> None:
-#     real_input = read_input(__file__)
-#     assert part_1(real_input) is not None
+@no_input_skip
+def test_part_1_real() -> None:
+    real_input = read_input(__file__)
+    assert part_1(real_input) == 1577255
 
 
-# @no_input_skip
-# def test_part_2_real() -> None:
-#     real_input = read_input(__file__)
-#     assert part_2(real_input) is not None
+@no_input_skip
+def test_part_2_real() -> None:
+    real_input = read_input(__file__)
+    assert part_2(real_input) == 1597035
 
 
 # -- Main
