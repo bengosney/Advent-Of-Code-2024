e96326fcbe22e31ab518360893cf3e80fc826ad9
Ben Gosney <bengosney@googlemail.com>
2024-12-18 09:13:14 +0000

    feat(day18): part 2

--- src/day_18.py
+++ src/day_18.py
@@ -1,45 +1,36 @@
-from collections import defaultdict
 from heapq import heappop, heappush
 
-from utils import no_input_skip, read_input  # noqa: F401
+from utils import no_input_skip, read_input
+
+
+class NoSolutionError(Exception):
+    pass
+
+
+class NoPathError(Exception):
+    pass
 
 
 def parse_input(puzzle: str) -> list[complex]:
     return [complex(int(x), int(y)) for x, y in (line.split(",") for line in puzzle.split("\n"))]
 
 
-def part_1(puzzle: str, size: int = 70, sim_len: int = 1024) -> int:  # noqa: C901
-    bytes = parse_input(puzzle)
-    grid = defaultdict(lambda: ".")
-
-    def draw_grid():
-        for y in range(size + 2):
-            for x in range(size + 2):
-                print(grid[complex(x, y)], end="")
-            print()
-
-    for byte in bytes[:sim_len]:
-        grid[byte] = "#"
-
+def walk_path(bytes: list[complex], size: int) -> set[complex]:
     visited: set[complex] = set()
     queue: list[tuple[int, int, int, set[complex]]] = []
 
-    best_score = 0
-    best_history = set()
     heappush(queue, (0, 0, 0, set([complex(0, 0)])))
     while queue:
         dist, x, y, history = heappop(queue)
         pos = complex(x, y)
 
-        if pos in visited or pos in bytes[:sim_len]:
+        if pos in visited or pos in bytes:
             continue
 
         visited.add(pos)
 
         if x == size and y == size:
-            best_score = dist
-            best_history = history
-            break
+            return history
 
         for direction in [complex(0, -1), complex(1, 0), complex(0, 1), complex(-1, 0)]:
             next_pos = pos + direction
@@ -50,16 +41,33 @@ def part_1(puzzle: str, size: int = 70, sim_len: int = 1024) -> int:  # noqa: C9
             if next_pos not in visited:
                 heappush(queue, (dist + 1, int(next_pos.real), int(next_pos.imag), history | {pos}))
 
-    for pos in best_history:
-        grid[pos] = "O"
-
-    # draw_grid()
-
-    return best_score
+    raise NoPathError()
 
 
-def part_2(puzzle: str) -> int:
-    pass
+def part_1(puzzle: str, size: int = 70, sim_len: int = 1024) -> int:
+    bytes = parse_input(puzzle)
+    path = walk_path(bytes[:sim_len], size)
+
+    return len(path)
+
+
+def part_2(puzzle: str, size: int = 70, inital_sim_length: int = 1024) -> str:
+    bytes = parse_input(puzzle)
+    sim_length = inital_sim_length
+
+    for _ in range(len(bytes) - inital_sim_length):
+        try:
+            path = walk_path(bytes[:sim_length], size)
+            for i in range(sim_length, len(bytes)):
+                if bytes[i] in path:
+                    sim_length = i + 1
+                    break
+        except NoPathError:
+            break
+    else:
+        raise NoSolutionError()
+
+    return f"{int(bytes[sim_length-1].real)},{int(bytes[sim_length-1].imag)}"
 
 
 # -- Tests
@@ -98,21 +106,21 @@ def test_part_1() -> None:
     assert part_1(test_input, 6, 12) == 22
 
 
-# def test_part_2() -> None:
-#     test_input = get_example_input()
-#     assert part_2(test_input) is not None
+def test_part_2() -> None:
+    test_input = get_example_input()
+    assert part_2(test_input, 6, 12) == "6,1"
 
 
-# @no_input_skip
-# def test_part_1_real() -> None:
-#     real_input = read_input(__file__)
-#     assert part_1(real_input) is not None
+@no_input_skip
+def test_part_1_real() -> None:
+    real_input = read_input(__file__)
+    assert part_1(real_input) == 348
 
 
-# @no_input_skip
-# def test_part_2_real() -> None:
-#     real_input = read_input(__file__)
-#     assert part_2(real_input) is not None
+@no_input_skip
+def test_part_2_real() -> None:
+    real_input = read_input(__file__)
+    assert part_2(real_input) == "54,44"
 
 
 # -- Main
