b8e43a5f64cc2539fc3bfc9a7e482008858a2fd4
Ben Gosney <bengosney@googlemail.com>
2024-12-20 20:44:11 +0000

    feat(day20): part 2

--- src/day_20.py
+++ src/day_20.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from functools import lru_cache
 from itertools import combinations
 
 import pytest
@@ -29,17 +30,17 @@ def parse_input(puzzle: str) -> list[complex]:
     return path
 
 
+@lru_cache
 def manhattan_distance(a: complex, b: complex) -> int:
     return int(abs(a.real - b.real) + abs(a.imag - b.imag))
 
 
-def count_cheets(path: list[complex]) -> dict[int, int]:
+def count_cheets(path: list[complex], max_skip: int, min_skip: int) -> dict[int, int]:
     cheats = defaultdict(int)
+    index_map = {point: index for index, point in enumerate(path)}
     for a, b in combinations(path, 2):
-        if manhattan_distance(a, b) <= 2:
-            a_index = path.index(a)
-            b_index = path.index(b)
-            diff = (b_index - a_index) - 2
+        if (distance := manhattan_distance(a, b)) <= max_skip:
+            diff = (index_map[b] - index_map[a]) - distance
             if diff > 0:
                 cheats[diff] += 1
 
@@ -48,13 +49,16 @@ def count_cheets(path: list[complex]) -> dict[int, int]:
 
 def part_1(puzzle: str) -> int:
     path = parse_input(puzzle)
-    cheats = count_cheets(path)
+    cheats = count_cheets(path, 2, 100)
 
     return sum(count for saving, count in cheats.items() if saving >= 100)
 
 
 def part_2(puzzle: str) -> int:
-    pass
+    path = parse_input(puzzle)
+    cheats = count_cheets(path, 20, 100)
+
+    return sum(count for saving, count in cheats.items() if saving >= 100)
 
 
 # -- Tests
@@ -83,13 +87,32 @@ def test_part_1() -> None:
     output = {2: 14, 4: 14, 6: 2, 8: 4, 10: 2, 12: 3, 20: 1, 36: 1, 38: 1, 40: 1, 64: 1}
 
     path = parse_input(test_input)
-    cheats = count_cheets(path)
+    cheats = count_cheets(path, 2, 1)
     assert cheats == output
 
 
-# def test_part_2() -> None:
-#     test_input = get_example_input()
-#     assert part_2(test_input) is not None
+def test_part_2() -> None:
+    test_input = get_example_input()
+    output = {
+        50: 32,
+        52: 31,
+        54: 29,
+        56: 39,
+        58: 25,
+        60: 23,
+        62: 20,
+        64: 19,
+        66: 12,
+        68: 14,
+        70: 12,
+        72: 22,
+        74: 4,
+        76: 3,
+    }
+    path = parse_input(test_input)
+    cheats = count_cheets(path, 20, 50)
+    over_50 = {saving: count for saving, count in cheats.items() if saving >= 50}
+    assert over_50 == output
 
 
 @no_input_skip
@@ -99,10 +122,11 @@ def test_part_1_real() -> None:
     assert part_1(real_input) == 1369
 
 
-# @no_input_skip
-# def test_part_2_real() -> None:
-#     real_input = read_input(__file__)
-#     assert part_2(real_input) is not None
+@no_input_skip
+@pytest.mark.slow
+def test_part_2_real() -> None:
+    real_input = read_input(__file__)
+    assert part_2(real_input) == 979012
 
 
 # -- Main
