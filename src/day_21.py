956d30cdddc99f28b5017bedd732e26e2a4525d5
Ben Gosney <bengosney@googlemail.com>
2025-01-05 20:50:21 +0000

    wip(day21): part 2 should work, not enough caching

--- src/day_21.py
+++ src/day_21.py
@@ -1,11 +1,22 @@
 from collections.abc import Generator
+from functools import cache
 from heapq import heappop, heappush
 from itertools import pairwise
 from typing import Literal
 
 import pytest
 
-from utils import no_input_skip, read_input
+from utils import NoSolutionError, no_input_skip, read_input
+
+
+class FixedHashableDict(dict):
+    def __init__(self, id: str, *args, **kwargs):
+        self._hash = hash(id)
+        super().__init__(*args, **kwargs)
+
+    def __hash__(self):
+        return self._hash
+
 
 KeypadKeys = Literal["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "A"]
 DirectionKeys = Literal["<", ">", "^", "v", "A"]
@@ -13,22 +24,18 @@ Point = tuple[int, int]
 Keypad = dict[KeypadKeys, Point] | dict[DirectionKeys, Point]
 
 # fmt: off
-numeric_keypad: dict[Point, KeypadKeys] = {
-    (0, 0): '7', (1, 0): '8', (2, 0): '9',
-    (0, 1): '4', (1, 1): '5', (2, 1): '6',
-    (0, 2): '1', (1, 2): '2', (2, 2): '3',
-    (1, 3): '0', (2, 3): 'A'
-}
-# fmt: on
-numeric_keypad_reversed: dict[KeypadKeys, Point] = {v: k for k, v in numeric_keypad.items()}
+numeric_keypad: dict[KeypadKeys, Point] = FixedHashableDict('numeric', {
+    '7': (0, 0), '8': (1, 0), '9':(2, 0),
+    '4': (0, 1), '5': (1, 1), '6':(2, 1),
+    '1': (0, 2), '2': (1, 2), '3':(2, 2),
+    '0': (1, 3), 'A': (2, 3)
+})
 
-# fmt: off
-directional_keypad: dict[Point, DirectionKeys] = {
-    (1, 0): '^', (2, 0): 'A',
-    (0, 1): '<', (1, 1): 'v', (2, 1): '>'
-}
+directional_keypad: dict[DirectionKeys, Point] = FixedHashableDict('directional', {
+    '^': (1, 0), 'A': (2, 0),
+    '<': (0, 1), 'v': (1, 1), '>': (2, 1),
+})
 # fmt: on
-directional_keypad_reversed: dict[DirectionKeys, Point] = {v: k for k, v in directional_keypad.items()}
 
 directions = [
     (-1, 0, "<", 1),
@@ -41,6 +48,7 @@ directions = [
 def find_paths(start: Point, end: Point, keypad: Keypad) -> Generator[str, None, None]:
     queue: list[tuple[int, int, int, str, set[Point]]] = []
     heappush(queue, (0, start[0], start[1], "", {start}))
+    keypad_values = set(keypad.values())
 
     while queue:
         dist, x, y, path, visited = heappop(queue)
@@ -50,25 +58,31 @@ def find_paths(start: Point, end: Point, keypad: Keypad) -> Generator[str, None,
 
         for dir_x, dir_y, move, _ in directions:
             new_x, new_y = x + dir_x, y + dir_y
-            if (new_x, new_y) in keypad.values() and (new_x, new_y) not in visited:
+            if (new_x, new_y) in keypad_values and (new_x, new_y) not in visited:
                 score = 1 if move == (path or "n")[-1] else 10
                 heappush(queue, (dist + score, new_x, new_y, path + move, {*visited, (new_x, new_y)}))
 
 
+@cache
 def get_path_between(start: str, end: str, keypad: Keypad, keypad_count: int) -> str:
     if not keypad_count:
         return "A"
 
     paths = []
-    shortest_path = "A" * 99999
+    shortest_path = None
     for path in find_paths(keypad[start], keypad[end], keypad):
-        got_path = get_path(directional_keypad_reversed, path, keypad_count - 1)
+        got_path = get_path(directional_keypad, path, keypad_count - 1)
         paths.append(got_path)
-        if len(got_path) < len(shortest_path):
+        if shortest_path is None or len(got_path) < len(shortest_path):
             shortest_path = got_path
+
+    if shortest_path is None:
+        raise NoSolutionError()
+
     return shortest_path
 
 
+@cache
 def get_path(keypad: Keypad, code: str, keypad_count: int) -> str:
     path = ""
     for a, b in pairwise("A" + code):
@@ -77,7 +91,7 @@ def get_path(keypad: Keypad, code: str, keypad_count: int) -> str:
 
 
 def get_move_list(code: str) -> str:
-    return get_path(numeric_keypad_reversed, code, 3)
+    return get_path(numeric_keypad, code, 3)
 
 
 def part_1(puzzle: str) -> int:
@@ -85,14 +99,21 @@ def part_1(puzzle: str) -> int:
     total_complexity = 0
 
     for code in codes:
-        moves = get_path(numeric_keypad_reversed, code, 3)
+        moves = get_path(numeric_keypad, code, 3)
         total_complexity += len(moves) * int(code[:-1])
 
     return total_complexity
 
 
 def part_2(puzzle: str) -> int:
-    pass
+    codes = puzzle.splitlines()
+    total_complexity = 0
+
+    for code in codes:
+        moves = get_path(numeric_keypad, code, 26)
+        total_complexity += len(moves) * int(code[:-1])
+
+    return total_complexity
 
 
 # -- Tests
@@ -143,21 +164,16 @@ def test_real_single_code(test_input, expected):
     assert len(moves) == expected
 
 
-# def test_part_2() -> None:
-#     test_input = get_example_input()
-#     assert part_2(test_input) is not None
-
-
 @no_input_skip
 def test_part_1_real() -> None:
     real_input = read_input(__file__)
     assert part_1(real_input) == 128962
 
 
-# @no_input_skip
-# def test_part_2_real() -> None:
-#     real_input = read_input(__file__)
-#     assert part_2(real_input) is not None
+@no_input_skip
+def test_part_2_real() -> None:
+    real_input = read_input(__file__)
+    assert part_2(real_input) == 33033749109056
 
 
 # -- Main
